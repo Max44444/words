60910ddd86b2288b05fdefd5d0b80fe01f803aaa
Maxim <skorokhodov.maxim.444@gmail.com>
2021-07-22 08:14:54 +0300

    create WordController with POST point /words/check

---
+++ src/main/java/com/example/words/service/WordsService.java
@@ -0,0 +1,11 @@
+package com.example.words.service;
+
+import com.example.words.dto.WordsDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordsService {
+    public WordsDto getCorrectWordsSequence(WordsDto wordsDto) {
+        return null;
+    }
+}
