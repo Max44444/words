e7ded458472800d73b012b17746da83c3816346a
Maxim <skorokhodov.maxim.444@gmail.com>
2021-07-26 11:54:32 +0300

    add WordsController tests

--- src/main/java/com/example/words/controller/WordsController.java
+++ src/main/java/com/example/words/controller/WordsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/words")
+@RestController
 public class WordsController {
 
     @Autowired
